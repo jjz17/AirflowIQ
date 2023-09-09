c9df002ca72a9a315143bf813831d76f9518044c
Jason Zhang <jasonjzhang17@gmail.com>
2023-09-09 17:10:56 -0400

    Initial air quality pipeline setup

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # AirflowIQ
 Real-Time Air Quality Insights Data Pipeline
+
+## Usage
+### Initial Setup
+1. (Download docker-compose file for airflow) ```curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.3.3/docker-compose.yaml'```
+2. (Create project directories) ```mkdir -p ./dags ./logs ./plugins ./processed_data ./raw_data```
+3. (Generate env file) ```echo -e "AIRFLOW_UID=$(id -u)" > .env```
+4. (Set up virtual environment)
+   1. ```python3 -m venv .venv```
+   2. ```. .venv/bin/activate```
+   3. ```pip install -r ./requirements.txt```
+5. (Set up databases and create user account) ```docker-compose up airflow-init```
+
+### General Setup
+1. (Start all services) ```docker-compose up```
+   1. (Monitor container health in another terminal window) ```docker ps```
+2. Visit [airflow UI](http://localhost:8080) with default credentials (username/password): airflow/airflow
+3. (Shut down all services) ```docker-compose down```
---
+++ dags/air_quality_pipeline.py
@@ -0,0 +1,105 @@
+from airflow import DAG
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+import json
+import pandas as pd
+import numpy as np
+
+# Define the DAG configuration
+default_args = {
+    "owner": "jason_zhang",
+    "start_date": days_ago(1),
+    "schedule_interval": "0 * * * *",  # Run hourly (Cronjob scheduling)
+}
+
+dag = DAG(
+    "air_quality_pipeline",
+    default_args=default_args,
+    description="Air Quality Data Pipeline",
+    catchup=False,  # Set to False to skip historical runs on first execution
+    tags=["data-pipeline"]
+)
+
+# Define the OpenAQ API endpoint for real-time air quality data
+openaq_api_url = "https://api.openaq.org/v1/measurements?country=YOUR_COUNTRY&city=YOUR_CITY&parameter=YOUR_PARAMETER"
+
+# Task to extract real-time air quality data
+def extract_data():
+    # Use the SimpleHttpOperator to make the API request
+    response = SimpleHttpOperator(
+        task_id="extract_air_quality_data",
+        http_conn_id="openaq_api_conn",  # Create a connection in Airflow to store API credentials
+        endpoint=openaq_api_url,
+        method="GET",
+        headers={"Content-Type": "application/json"},
+    )
+    return json.loads(response.text)
+
+# Task to transform the data
+def transform_data(data):
+    # Example data transformation: Convert JSON to a DataFrame
+    df = pd.DataFrame(data["results"])
+    # Additional transformations can be applied here
+    return df
+
+# Task to perform data analysis
+def analyze_data(df):
+    # Example data analysis: Calculate average air quality
+    average_air_quality = df["value"].mean()
+    return average_air_quality
+
+# Task to trigger alerts based on analysis
+def trigger_alerts(average_air_quality):
+    # Example: Send an alert if air quality drops below a threshold
+    if average_air_quality < 50:
+        # Implement your alerting logic here
+        print("Alert: Air quality is below the threshold!")
+
+# Task to generate reports
+def generate_report(df):
+    # Example: Generate a simple report
+    report = df.head()
+    # Additional reporting logic can be applied here
+    print(report)
+
+# Define the task dependencies
+data_extraction_task = PythonOperator(
+    task_id="extract_data",
+    python_callable=extract_data,
+    dag=dag,
+)
+
+data_transformation_task = PythonOperator(
+    task_id="transform_data",
+    python_callable=transform_data,
+    provide_context=True,
+    dag=dag,
+)
+
+data_analysis_task = PythonOperator(
+    task_id="analyze_data",
+    python_callable=analyze_data,
+    provide_context=True,
+    dag=dag,
+)
+
+alerting_task = PythonOperator(
+    task_id="trigger_alerts",
+    python_callable=trigger_alerts,
+    provide_context=True,
+    dag=dag,
+)
+
+reporting_task = PythonOperator(
+    task_id="generate_report",
+    python_callable=generate_report,
+    provide_context=True,
+    dag=dag,
+)
+
+# Define task dependencies
+data_extraction_task >> data_transformation_task >> data_analysis_task >> alerting_task >> reporting_task
+
+if __name__ == "__main__":
+    dag.cli()
